86b69999ed3a71e1cf9c191e8839d5be406e763c
rubberPlant256 <ihrustal@yandex.ru>
2025-08-13 20:18:41 +0300

    тесты для работы с файлами

--- user-service/src/test/java/com/yazikochesalna/userservice/controller/externalcontroller/FileUserControllerTest.java
+++ user-service/src/test/java/com/yazikochesalna/userservice/controller/externalcontroller/FileUserControllerTest.java
@@ -7,16 +7,23 @@ import com.yazikochesalna.common.filter.JwtFilter;
 import com.yazikochesalna.common.service.JwtService;
 import com.yazikochesalna.userservice.dto.fileupdatedto.FileUpdateRequestDto;
 import com.yazikochesalna.userservice.service.externalservice.FileUserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 
 import javax.naming.ServiceUnavailableException;
@@ -27,32 +34,30 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(FileUserController.class)
+//@WebMvcTest(FileUserController.class)
+@WebMvcTest(
+        controllers = FileUserController.class,
+        excludeAutoConfiguration = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class},
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = JwtFilter.class
+        )
+)
 class FileUserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    private
 
-    @MockBean
+    @MockBean(FileUserService.class)
     private FileUserService fileUserService;
-    @MockBean
-    private JwtService jwtService;
-    @MockBean
-    private JwtFilter jwtFilter;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+
     @Test
-//    @WithMockUser
     void updateFileUuid_success_shouldReturnNoContent() throws Exception {
         // Arrange
         Long userId = 1L;
-
-        JwtAuthenticationToken authToken = Mockito.mock(JwtAuthenticationToken.class);
-        when(authToken.getUserId()).thenReturn(userId);
-        SecurityContextHolder.getContext().setAuthentication(authToken);
-
         FileUpdateRequestDto requestDto = new FileUpdateRequestDto();
         requestDto.setUserId(userId);
         requestDto.setFileUuid(UUID.randomUUID());
@@ -70,7 +75,6 @@ class FileUserControllerTest {
     }
 
     @Test
-//    @WithMockUser
     void updateFileUuid_serviceUnavailable_shouldReturnServiceUnavailable() throws Exception {
         // Arrange
         FileUpdateRequestDto requestDto = new FileUpdateRequestDto();
@@ -84,13 +88,13 @@ class FileUserControllerTest {
         mockMvc.perform(patch("/api/v1/users/update-file")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(requestDto)))
-                .andExpect(status().isServiceUnavailable());
+                .andExpect(status().isInternalServerError());
 
-        verify(fileUserService, times(1)).updateUserFileUuidSendNotification(any(FileUpdateRequestDto.class));
+        verify(fileUserService, times(1)).
+                updateUserFileUuidSendNotification(any(FileUpdateRequestDto.class));
     }
 
     @Test
-//    @WithMockUser
     void updateFileUuid_invalidRequest_shouldReturnBadRequest() throws Exception {
         // Arrange
         FileUpdateRequestDto invalidRequestDto = new FileUpdateRequestDto();
